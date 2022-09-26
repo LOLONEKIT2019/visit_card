924f24a67fea8748b57d0b62f4ef644a393cc261
mikitabasalai <nikita.bac@mail.ru>
2022-09-26 11:47:39 +0200

    first version

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module visit_card
+module github.com/LOLONEKIT2019/visit_card
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,1 +1,43 @@
-package visit_card
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+const (
+	contentIndex  = "./content/index.html"
+	contentImages = "./content/images"
+
+	tlsCrt = "./tls/server.crt"
+	tlsKey = "./tls/server.key"
+
+	port = ":8080"
+)
+
+var (
+	tpl = template.Must(template.ParseFiles(contentIndex))
+)
+
+func main() {
+
+	r := mux.NewRouter()
+	r.HandleFunc(
+		"/",
+		func(w http.ResponseWriter, r *http.Request) {
+			tpl.Execute(w, nil)
+		},
+	)
+
+	r.HandleFunc(
+		"/{path}",
+		func(w http.ResponseWriter, r *http.Request) {
+			http.ServeFile(w, r, fmt.Sprintf("%s/%s", contentImages, mux.Vars(r)["path"]))
+		},
+	)
+
+	http.ListenAndServeTLS(port, tlsCrt, tlsKey, r)
+}
